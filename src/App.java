5f692885077bfb4a141fa70d0aef285fcae8f920
Gabriel-Paes <paessouzagabriel@gmail.com>
2022-10-07 21:21:10 -0300

    Refactoring the function

--- src/App.java
+++ src/App.java
@@ -3,23 +3,22 @@ import java.util.Scanner;
 public class App {
     public static void main(String[] args) throws Exception {
         Scanner sc = new Scanner(System.in);
-        
-        
-        System.out.print("Informe o 1 valor: ");
+     
+        System.out.print("Informe o 1º valor: ");
         int n1 = sc.nextInt();
 
-        System.out.print("Informe o 2 valor: ");
+        System.out.print("Informe o 2º valor: ");
         int n2 = sc.nextInt();
 
-        System.out.print("Informe o 3 valor: ");
+        System.out.print("Informe o 3º valor: ");
         int n3 = sc.nextInt();
         
         sc.close();
 
-        somaValores(n1, n2, n3);
+        somar(n1, n2, n3);
     }
-    public static void somaValores(int n1, int n2, int n3){
-        int soma = n1+n2+n3;
+    public static void somar(int n1, int n2, int n3){
+        int soma = n1 + n2 + n3;
         System.out.println("A soma é "+soma);
     }
 }
