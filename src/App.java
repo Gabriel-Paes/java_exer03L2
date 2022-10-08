c5ea00c3a09f40383fe5e7f34b65d213422f3517
Gabriel-Paes <paessouzagabriel@gmail.com>
2022-10-07 21:11:57 -0300

    Function somaValores

--- src/App.java
+++ src/App.java
@@ -1,5 +1,25 @@
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Scanner sc = new Scanner(System.in);
+        
+        
+        System.out.print("Informe o 1 valor: ");
+        int n1 = sc.nextInt();
+
+        System.out.print("Informe o 2 valor: ");
+        int n2 = sc.nextInt();
+
+        System.out.print("Informe o 3 valor: ");
+        int n3 = sc.nextInt();
+        
+        sc.close();
+
+        somaValores(n1, n2, n3);
+    }
+    public static void somaValores(int n1, int n2, int n3){
+        int soma = n1+n2+n3;
+        System.out.println("A soma é "+soma);
     }
 }
